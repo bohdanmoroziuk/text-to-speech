090965e7361341ff83f0f7a488b7fd3bf82159d3
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-20 18:03:36 +0300

    Import style file

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
+
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 import './index.css';
 
 render(<App />, document.getElementById('root'));
