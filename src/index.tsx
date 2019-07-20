c87b648caf61b5320935e1c7c4e00ee26f863849
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-20 18:04:11 +0300

    Update paths

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import App from './App';
+import App from 'App';
 
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
-import './index.css';
+import 'index.css';
 
 render(<App />, document.getElementById('root'));
 
