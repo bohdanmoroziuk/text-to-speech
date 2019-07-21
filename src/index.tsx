fc2076c0637cf7e35f1a8c33d93af9e5c1597309
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-21 11:14:02 +0300

    Set up a redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import store from 're-ducks/store';
+
 import App from 'App';
 
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 import 'index.css';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider {...{store}}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
